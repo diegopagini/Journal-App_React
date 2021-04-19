e85f0549963b87ea80749bc35edc20ad161c2cb9
Diego Pagini <dq@qbkconsulting.com>
2021-04-19 08:01:25 -0300

    estilos del login

--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -2,17 +2,20 @@
 
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { RegisterScreen } from '../components/auth/RegisterScreen';
 
 export const AuthRouter = () => {
 	return (
-		<div>
-			<Switch>
-				<Route exact path='/auth/login' component={LoginScreen} />
-				<Route exact path='/auth/register' component={RegisterScreen} />
-				<Redirect to='/auth/login' />
-			</Switch>
+		<div className='auth__main'>
+			<div className='auth__box-container'>
+				<Switch>
+					<Route exact path='/auth/login' component={LoginScreen} />
+					<Route exact path='/auth/register' component={RegisterScreen} />
+					<Redirect to='/auth/login' />
+				</Switch>
+			</div>
 		</div>
 	);
 };
