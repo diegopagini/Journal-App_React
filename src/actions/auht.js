cfb2ed10f415fc100623965fe21797cf97712f38
Diego Pagini <dq@qbkconsulting.com>
2021-04-20 08:22:40 -0300

    dispatch de login

---
+++ src/actions/auht.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import { types } from '../types/types';
+
+export const login = (uid, displayName) => {
+	return {
+		type: types.login,
+		payload: {
+			uid,
+			displayName,
+		},
+	};
+};
--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,25 +1,46 @@
 /** @format */
 
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+import { login } from '../../actions/auht';
 
 export const LoginScreen = () => {
+	const dispatch = useDispatch();
+
+	const [formValues, handleInputChange] = useForm({
+		email: 'diego@gmail.com',
+		password: '123456',
+	});
+
+	const { email, password } = formValues;
+
+	const handleLogin = (e) => {
+		e.preventDefault();
+		dispatch(login(12345, 'Nando'));
+	};
+
 	return (
 		<>
 			<h3 className='auth__title'>Login</h3>
-			<form>
+			<form onSubmit={handleLogin}>
 				<input
 					className='auth__input'
 					type='text'
 					placeholder='Email'
 					name='email'
 					autoComplete='off'
+					value={email}
+					onChange={handleInputChange}
 				/>
 				<input
 					className='auth__input'
 					type='password'
 					placeholder='Password'
 					name='password'
+					value={password}
+					onChange={handleInputChange}
 				/>
 				<button type='submit' className='btn btn-primary btn-block'>
 					Login
